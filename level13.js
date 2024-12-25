af551e18616387bc385b36d13456248144d5fa43
Kaylin Khanal <kyalinnop@gmail.com>
2024-12-25 09:07:58 +0545

    add new question level 13

---
+++ level13.js
@@ -0,0 +1,17 @@
+const name = 'ram thapa'
+
+//Q1. Expected output:
+// ['thapa','ram']
+
+
+
+//Q2. Expected output
+//  {
+// 'ram': 'thapa'
+// }
+
+
+//Q3. expected output: 
+// {
+    // username: 'Ram Thapa'
+// },
