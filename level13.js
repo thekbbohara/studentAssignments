21e3e4793e55efb88ed7b87674f1b56d26dc1655
Kaylin Khanal <kyalinnop@gmail.com>
2024-12-29 08:26:47 +0545

    added answers

--- level13.js
+++ level13.js
@@ -2,16 +2,22 @@ const name = 'ram thapa'
 
 //Q1. Expected output:
 // ['thapa','ram']
-
-
-
+console.log(name.split(' ').reverse())
 //Q2. Expected output
 //  {
 // 'ram': 'thapa'
 // }
 
-
+const firstName = name.split(' ')[0]
+const lastName = name.split(' ')[1]
+console.log({
+   [firstName]: lastName
+})
 //Q3. expected output: 
 // {
     // username: 'Ram Thapa'
 // },
+
+console.log({
+    username: firstName[0].toUpperCase() + firstName.substring(1) + ' ' + lastName[0].toUpperCase() + lastName.substring(1)
+})
