5d2fa81bff332146765d0282bcae3cf5bfc86832
Kaylin Khanal <kyalinnop@gmail.com>
2024-12-29 08:33:07 +0545

    added level 14 array

--- level12.js
+++ level12.js
@@ -32,18 +32,18 @@ console.log(`${name} own ${car} at age ${age}`)
 
 
 
-1. what is git? 
-    version control system
-2. what is github?
-    cloud based platform that uses git as version control to store our codebase
-3. alternative: bitbucket, gitlab, azure repos
-4. Why Github?
-    - Maximum company uses github
-    - Maintain releases, collab within the team...
-    - Open source
-    - Github student package
-5. Repository: Space to upload your project code
-5. Branches in Github: features are branches
+// 1. what is git? 
+//     version control system
+// 2. what is github?
+//     cloud based platform that uses git as version control to store our codebase
+// 3. alternative: bitbucket, gitlab, azure repos
+// 4. Why Github?
+//     - Maximum company uses github
+//     - Maintain releases, collab within the team...
+//     - Open source
+//     - Github student package
+// 5. Repository: Space to upload your project code
+// 5. Branches in Github: features are branches
 
 
 
