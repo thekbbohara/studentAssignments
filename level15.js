a666c782900f33192ca8709ee9139f5d38a2d725
Kaylin Khanal <kyalinnop@gmail.com>
2024-12-29 09:13:19 +0545

    add level 15 array and obj

---
+++ level15.js
@@ -0,0 +1,21 @@
+const details = {
+    user: {
+        name: 'ram khanal'
+    },
+    subjects: ['math','science']
+}
+
+const subjects= {
+    math: 35,
+    science: 36,
+    computer: 56
+}
+
+
+//Expected output: 
+// {
+//     user: {
+//         name: 'ram khanal'
+//     },
+//     subjects: [35,36]
+// }
