57d52037de1a484c00a6661e0fd3879311facbf7
Kaylin Khanal <kyalinnop@gmail.com>
2024-12-30 08:29:47 +0545

    add level 17 min max delete merge array

---
+++ level17.js
@@ -0,0 +1,22 @@
+
+const results = {
+    2023: [23,12,32],
+    2024: [42,54,43],
+    2025: [0,0,0]
+}
+
+//Q1 find  the all time highest score hint: use Math.max
+//expected ouput is 54
+
+
+
+//Q2 remove 2025
+//exepcted output {
+//     2023: [23,12,32],
+//     2024: [42,54,43],
+// }
+
+
+//Q3 add all scores in a array
+// [23,12,32,42,54,43]
+
