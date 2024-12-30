6eebae8981054aeccd84c6dd0676a2f3896b4859
Kaylin Khanal <kyalinnop@gmail.com>
2024-12-30 08:16:27 +0545

    added level 16 push and pop

---
+++ level16.js
@@ -0,0 +1,17 @@
+
+const laps = ['10km', '12km', '13km']
+
+//Q1. expected output = 
+// {
+//   totalLap: 35
+// }
+
+const newLap = '14km'
+const newLap2 = '11km'
+// Q2. console.log laps, expected output 
+// ['10km', '12km', '13km', '14km','11km']
+
+
+//Q3. pop last lap from the array
+//  console.log laps, expected output 
+// ['10km', '12km', '13km', '14km']
