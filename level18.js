9dc88e0a96755644ddac84c2f656e44759fbd1f2
thekbbohara <ytkbzcraft@gmail.com>
2025-01-01 09:04:43 +0545

    q1n2

--- level18.js
+++ level18.js
@@ -1,15 +1,22 @@
 let pocketMoney= 200
 
-function buyChocolate(price, chocolate){
-    const remainingAmount = pocketMoney - price
-    return 'If you buy cholocate' + chocolate + 'you will have '+ remainingAmount
-}
-
-console.log(buyChocolate(10, 'kitkat')) //arguments
-console.log(buyChocolate(20, 'dairymilk'))
-
-
-
 //Q1. expected output: 
 //You can buy 20 kitkat with your pocketmoney
 // You can buy 10 dairymilk with your pocketmoney
+
+
+const buyChocolate = (price, chocolate,quantity) => {
+  if (pocketMoney < price) return `you are short on rs ${price-pocketMoney}`
+  if(quantity) {
+    const remainingMoney = pocketMoney - (quantity * price);
+    if (pocketMoney < price*quantity) return `you are short on rs ${price*quantity -pocketMoney}`
+    pocketMoney = remainingMoney 
+    return `You will have rs ${remainingMoney} remaining.`;
+  }
+  const _quantity = Math.floor(pocketMoney / price);
+  const remainingMoney = pocketMoney - (_quantity * price);
+  pocketMoney = remainingMoney 
+  return `You can buy ${_quantity} ${chocolate}(s) with your pocket money, and the remaining money is ${remainingMoney}.`;
+};
+
+console.log(buyChocolate(11, "KitKat",2));
