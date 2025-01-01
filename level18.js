6feaeaa6c9a18b1ed6923888cc067060c51aa379
thekbbohara <ytkbzcraft@gmail.com>
2025-01-01 09:20:07 +0545

    add odd num

--- level18.js
+++ level18.js
@@ -3,21 +3,6 @@ let pocketMoney= 200
 //You can buy 20 kitkat with your pocketmoney
 // You can buy 10 dairymilk with your pocketmoney
 
-
-const buyChocolate = (price, chocolate,quantity) => {
-  if (pocketMoney < price) return `you are short on rs ${price-pocketMoney}`
-  if(quantity) {
-    const remainingMoney = pocketMoney - (quantity * price);
-    if (pocketMoney < price*quantity) return `you are short on rs ${price*quantity -pocketMoney}`
-    pocketMoney = remainingMoney 
-    return `You will have rs ${remainingMoney} remaining.`;
-  }
-  const _quantity = Math.floor(pocketMoney / price);
-  const remainingMoney = pocketMoney - (_quantity * price);
-  pocketMoney = remainingMoney 
-  return `You can buy ${_quantity} ${chocolate}(s) with your pocket money, and the remaining money is ${remainingMoney}.`;
-};
-
 // console.log(buyChocolate(11, "KitKat",2));
 //Q2. 
 console.log(buyChocolate(10, 'kitkat', 3)) //arguments
--- level19.js
+++ level19.js
@@ -1,8 +1,9 @@
 const arr = [3,5,6,1]
 let sum = 0
+
+// calculate sum of odd number
 arr.forEach((item)=>{
   if(item%2!==0) sum= sum + item
 })
 console.log(sum)
 
-// calculate sum of odd number
